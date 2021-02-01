1c36771ec2cba2e15f9fd0e700dee9856a46b205
Matthew Lymer <matthew.lymer@tripstack.com>
2021-01-31 22:01:23 -0500

    Fix up docker compose, remove redundant files, update homepage blerb

--- src/Lymer.Web.App/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Lymer.Web.App.Models
-{
-    public sealed class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
