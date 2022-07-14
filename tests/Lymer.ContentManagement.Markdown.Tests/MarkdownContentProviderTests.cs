96f4255a66271b9a6a468472f27401694ede1371
Matthew Lymer <matthew@lymer.ca>
2022-07-13 22:36:12 -0400

    Add niave implemention of yaml metadata

--- tests/Lymer.ContentManagement.Markdown.Tests/MarkdownContentProviderTests.cs
+++ tests/Lymer.ContentManagement.Markdown.Tests/MarkdownContentProviderTests.cs
@@ -1,9 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
 
 namespace Lymer.ContentManagement.Markdown.Tests
 {
@@ -21,20 +26,41 @@ namespace Lymer.ContentManagement.Markdown.Tests
         [Fact]
         public async Task ShouldReturnMetadataForArticles()
         {
+            const string fileContent = @"---
+title: This is my title
+description: |
+  This is my description,
+  isn't it great?
+tags:
+  - tag1
+  - tag2
+  - tag3
+---
+Hello World";
+            
             var fileProvider = new MemoryFileProvider();
-            fileProvider.AddFile("foo.md", @"");
+            fileProvider.AddFile("foo.md", fileContent);
+            
             var provider = new MarkdownContentProvider(fileProvider);
+            
             var articles = await provider.GetArticlesAsync(CancellationToken.None);
-            Assert.Collection(articles, x =>
-            {
-                Assert.Equal("Hello World", x.Title);
-            });
+            
+            Assert.Collection(
+                articles, 
+                x =>
+                {
+                    var metadata = x.Metadata;
+                    Assert.Equal("This is my title", metadata.Title);
+                    Assert.Equal("This is my description,\nisn't it great?\n", metadata.Description);
+                    Assert.True(metadata.Tags.SequenceEqual(new[] {"tag1", "tag2", "tag3"}));
+                });
         }
     }
 
     internal sealed class MemoryFileProvider : IFileProvider
     {
         private readonly List<(string Name, string Content)> _files = new List<(string Name, string Content)>();
+        
         public void AddFile(string name, string content)
         {
             _files.Add((name, content));
@@ -45,11 +71,21 @@ namespace Lymer.ContentManagement.Markdown.Tests
             await Task.CompletedTask;
             return _files.Select(x => x.Name).ToList().AsReadOnly();
         }
+
+        public async Task<Stream> OpenFileAsync(string name, CancellationToken cancellationToken)
+        {
+            await Task.CompletedTask;
+
+            var file = _files.Single(x => x.Name == name);
+
+            return new MemoryStream(Encoding.UTF8.GetBytes(file.Content));
+        }
     }
 
     internal interface IFileProvider
     {
         Task<IReadOnlyCollection<string>> GetFileNamesAsync(CancellationToken cancellationToken);
+        Task<Stream> OpenFileAsync(string name, CancellationToken cancellationToken);
     }
 
     internal sealed class MarkdownContentProvider
@@ -61,17 +97,46 @@ namespace Lymer.ContentManagement.Markdown.Tests
             _fileProvider = fileProvider ?? throw new ArgumentNullException(nameof(fileProvider));
         }
 
-        public async Task<IReadOnlyCollection<ArticleMetadata>> GetArticlesAsync(CancellationToken cancellationToken)
+        public async Task<IReadOnlyCollection<Article>> GetArticlesAsync(CancellationToken cancellationToken)
         {
-            return (await _fileProvider.GetFileNamesAsync(cancellationToken))
-                .Select(x => new ArticleMetadata())
-                .ToList()
-                .AsReadOnly();
+            var deserializer = new DeserializerBuilder()
+                .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                .Build();
+            
+            var result = new List<Article>();
+            
+            foreach (var fileName in await _fileProvider.GetFileNamesAsync(cancellationToken))
+            {
+                await using var stream = await _fileProvider.OpenFileAsync(fileName, cancellationToken);
+                using var reader = new StreamReader(stream);
+
+                var content = await reader.ReadToEndAsync();
+
+                var matches = Regex.Matches(content, "---(.*)---", RegexOptions.Singleline);
+
+                var metadata = deserializer.Deserialize<ArticleMetadata>(matches[0].Groups[1].Value);
+
+                var article = new Article
+                {
+                    Metadata = metadata
+                };
+                
+                result.Add(article);
+            }
+
+            return result.AsReadOnly();
         }
     }
 
-    internal sealed class ArticleMetadata
+    public sealed class Article
     {
-        public string Title => "Hello World";
+        public ArticleMetadata Metadata { get; set; }
+    }
+
+    public sealed class ArticleMetadata
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public List<string> Tags { get; set; }
     }
 }
