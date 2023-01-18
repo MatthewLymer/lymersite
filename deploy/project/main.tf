4da9cdf00bd1164ea9d73a547fa0a0e3e283a385
Matthew Lymer <matthew@lymer.ca>
2023-01-17 23:37:39 -0500

    Add github actions setup

--- deploy/project/main.tf
+++ deploy/project/main.tf
@@ -6,11 +6,24 @@ locals {
     id   = "matthewlymer-lymersite"
     name = "LymerSite"
   }
+
+  github = {
+    repository = "lymersite"
+  }
+}
+
+variable "github_token" {
+  type        = string
+  description = "The fine grained personal access token with read/write permissions for 'Secrets' on the target repository. See https://github.com/settings/tokens"
 }
 
 provider "google" {
   region  = local.region
   project = local.project.id
+}
+
+provider "github" {
+  token = var.github_token
 }
 
 resource "google_project" "default" {
@@ -23,5 +36,11 @@ terraform {
   backend "gcs" {
     bucket = "490635812867-tfstate"
     prefix = "matthewlymer-lymersite-project"
+  }
+
+  required_providers {
+    github = {
+      source = "integrations/github"
+    }
   }
 }
