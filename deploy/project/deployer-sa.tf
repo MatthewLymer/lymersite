342709e09c8f807e7d476c3871347ed2dd43ff27
Matthew Lymer <matthew@lymer.ca>
2023-08-22 21:21:12 -0400

    Add artifact repository role for deployer service account

--- deploy/project/deployer-sa.tf
+++ deploy/project/deployer-sa.tf
@@ -13,3 +13,11 @@ resource "google_storage_bucket_iam_member" "github_actions_deployer_tfstate_adm
 resource "google_service_account_key" "github_actions_deployer" {
   service_account_id = google_service_account.github_actions_deployer.name
 }
+
+resource "google_artifact_registry_repository_iam_member" "github_actions_deployer_default_writer" {
+  project    = local.project.id
+  location   = google_artifact_registry_repository.default.location
+  repository = google_artifact_registry_repository.default.name
+  role       = "roles/artifactregistry.writer"
+  member     = "serviceAccount:${google_service_account.github_actions_deployer.email}"
+}
